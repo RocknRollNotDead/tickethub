5f8f9df14c4204377b2e233f752cc4a0e79d693a
artemka2003 <86artem2003@mail.ru>
2026-09-14 21:34:49 +0300

    init commit

---
+++ src/test/java/ru/codeportfolio/tickethub/TickethubApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.codeportfolio.tickethub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickethubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
