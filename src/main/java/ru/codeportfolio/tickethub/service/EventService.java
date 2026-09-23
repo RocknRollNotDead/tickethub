da73f89f4b3d66f4c4ffb2a702c834bd1f29de24
artemka2003 <86artem2003@mail.ru>
2026-09-23 13:36:16 +0300

    feat: add cache for getEvents

--- src/main/java/ru/codeportfolio/tickethub/service/EventService.java
+++ src/main/java/ru/codeportfolio/tickethub/service/EventService.java
@@ -1,7 +1,10 @@
 package ru.codeportfolio.tickethub.service;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.map.CaseInsensitiveMap;
 import org.springframework.stereotype.Service;
 import ru.codeportfolio.tickethub.dto.EventRequestDto;
 import ru.codeportfolio.tickethub.dto.EventResponseDto;
@@ -11,6 +14,9 @@ import ru.codeportfolio.tickethub.repository.EventRepository;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -18,22 +24,44 @@ import java.util.List;
 public class EventService {
     private final EventRepository eventRepository;
     private final EventMapper eventMapper;
+    private final Cache<Long, Event> eventsCache = Caffeine.newBuilder()
+            .expireAfterAccess(30,TimeUnit.MINUTES)
+            .maximumSize(1000)
+            .build();
 
     public void createEvent(EventRequestDto eventRequestDto) {
         Event event = eventRepository.save(Event.builder()
-                        .name(eventRequestDto.name())
-                        .date(eventRequestDto.date())
-                        .totalSeats(eventRequestDto.totalSeats())
-                        .availableSeats(eventRequestDto.availableSeats())
-                        .build());
+                .name(eventRequestDto.name())
+                .date(eventRequestDto.date())
+                .totalSeats(eventRequestDto.totalSeats())
+                .availableSeats(eventRequestDto.availableSeats())
+                .build());
         log.info("Saved event {}, id: {}.", event.getName(), event.getId());
+        putEventsToCache(eventRepository.findAll());
     }
 
-    public List<EventResponseDto> getAllEvents(){
-        List<Event> events = eventRepository.findAll();
+    public List<EventResponseDto> getAllEvents() {
+        List<Event> events;
+        if (eventRepository.count() == eventsCache.estimatedSize()) {
+            events = List.of((Event) eventsCache.asMap().values());
+        } else {
+            events = eventRepository.findAll();
+        }
+
         List<EventResponseDto> eventResponseDtoList = eventMapper.toDtoList(events);
         eventResponseDtoList.sort(Comparator.comparing(EventResponseDto::date));
         return eventResponseDtoList;
 
     }
+
+    private void putEventsToCache(List<Event> events) {
+        eventsCache.putAll(
+                events.stream().collect(Collectors.toMap(
+                        Event::getId,
+                        Function.identity()
+                ))
+
+        );
+    }
+
 }
